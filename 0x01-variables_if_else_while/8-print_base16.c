d582639f26c5640f504aa573e643184060ac8549
MNG-06 <mphela.mminanare@gmail.com>
2023-03-21 03:12:00 +0200

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 58; i < 68; i++)
+	{
+		putchar(i);
+	}
+	for (i = 93; i < 107; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
