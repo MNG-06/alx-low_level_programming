d50c0b3f550b55830656158d70366978b9978d0e
MNG-06 <mphela.mminanare@gmail.com>
2023-03-22 02:14:43 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - creating the alphabets all in lower cases
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char v';
+
+	for (v' = 'a'; v' <= 'z'; v'++)
+	{
+		_putchar(v');
+	}
+
+	_putchar('\n');
+}
