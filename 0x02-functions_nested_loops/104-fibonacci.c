d4713e8bd1712ac8a1cf2f6697dd17839cd8fba5
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-03-22 18:14:08 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	unsigned long int u;
+	unsigned long int i;
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
-	unsigned long int v = 1000000000;
+	unsigned long int l = 1000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
@@ -21,22 +21,22 @@ int main(void)
 
 	printf("%lu", bef);
 
-	for (u = 1; u < 91; u++)
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
 
-	bef1 = (bef / v);
-	bef2 = (bef % v);
-	aft1 = (aft / v);
-	aft2 = (aft % v);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
-	for (u = 92; u < 99; ++u)
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % v);
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
@@ -45,4 +45,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
