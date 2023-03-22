0b773c75523df0038dec7bd22215191ff691b8a5
MNG-06 <mphela.mminanare@gmail.com>
2023-03-22 03:08:57 +0200

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_sign - printing the sign of a number
+  * @n: the number that checked
+  *
+  * Return: 1 for positive num, -1 for negative num or 0 for anyother thing
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
