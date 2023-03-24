f1da5e9e9ee223e8819d5a3797ca83de04535e86
MNG-06 <mphela.mminanare@gmail.com>
2023-03-24 16:48:19 +0200

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * print_line - Draws a straight line according to parameter
+  * @n: The number of lines to draw
+  *
+  * Return: empty
+  */
+void print_line(int n)
+{
+	_putchar(----------------)
+		_putchar('\n')
+}
