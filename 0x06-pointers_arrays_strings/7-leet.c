abb06a8b79ad193212a387b4609e0cbf229dfadd
MNG-06 <mphela.mminanare@gmail.com>
2023-03-30 23:26:01 +0200

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * leet - Encoding a string into 1337
+  * @s: The string to encode
+  *
+  * Return: The encoded string
+  */
+char *leet(char *s)
+{
+	int i = 0, j = 0, k = 5;
+	char m[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[i])
+	{
+		j = 0;
+
+		while (j < 1)
+		{
+			if (s[i] == m[j] || s[i] - 32 == m[j])
+			{
+				s[i] = n[j];
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
