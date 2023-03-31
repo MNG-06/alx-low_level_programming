4c6c95081704f30ac1241167e3a2f3d0b904b80a
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-03-31 23:31:50 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 
 	while (s[a])
 	{
-		while ((s[a] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[a] <= 'Z'))
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
 		{
 			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
 			{
