8e1238fa1fe6631014a345b6635ba97fe250a63d
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-04-06 02:45:28 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string, then followed by \n.
- * @s: is the string to be printed.
- *
- * Return: Nothing.
- */
+  * _puts_recursion - print a string, then followed by \n.
+  * @s: is the string to be printed.
+  *
+  * Return: Nothing.
+  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
