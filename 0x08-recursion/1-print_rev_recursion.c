d521bd0c78409a71707dc95a58234ea9c75ddb34
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-04-07 01:15:22 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Print a string in reverse
- * @s: The string that is to be reversed
- *
- * Return: Nothing.
- */
+  * _print_rev_recursion - Print a string in reverse
+  * @s: the string that is to reverse
+  *
+  * Return: Nothing.
+  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	
-		return 0;
+		return;
 	}
-	else
-		s++;
-		_print_rev_recursion(s);
-		s--;
-		_putchar(*s);
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
