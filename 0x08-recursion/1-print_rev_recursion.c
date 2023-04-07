e0a458440423b16d58f076a55c8096fdfe9e7686
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-04-07 02:05:13 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,10 +8,13 @@
   */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		return;
-	}
-	_print_rev_recursion(s++);
-	_putchar(*s);
+	 if (*s)
+        {
+
+                return;
+        }
+        else
+                s++;
+                _print_rev_recursion(s)
+                _putchar(*s);
 }
