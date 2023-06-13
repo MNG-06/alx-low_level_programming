14aefbce6dad98e468ff60131efb77f464762ff0
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-06-13 14:29:22 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Printing the name of the program
+ * main - Prints the name of the program
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 0
