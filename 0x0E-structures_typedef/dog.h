76a7081bfacbdb3ae184938bf4b75b3e68e722c6
MNG-06 <mphela.mminanare@gmail.com>
2023-06-19 21:41:42 +0200

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A simple struct that contains variables
+ * @name: Holds name of dog in char array
+ * @age: Holds number, including any decimals
+ * @owner: Holds name of owner in char array
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
