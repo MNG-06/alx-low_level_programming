63364223669dd30662f626bdf9db714384fe89c1
MNG-06 <mphela.mminanare@gmail.com>
2023-06-19 21:12:59 +0200

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that prints out the name of the file that is compiled from
+ * @void: Nothing
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
