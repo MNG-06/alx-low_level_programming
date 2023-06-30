1c2723294daddb092d8fc3669330cca0a42ebedf
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-06-30 17:51:04 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include <stddef.h>
 #include "lists.h"
-
 /**
- * list_len - a function that returns the number of elements linked.
- * @h: Pointer to the head of the linked list
- * Return: The total amount of nodes in the linked list
- */
+  * list_len - finds the number of nodes in a list
+  * @h: singly linked list
+  * Return: number of elements in a linked list
+  */
 size_t list_len(const list_t *h)
 {
 	int i;
 
-	if (h == NULL)
-		return (0);
-	for (i = 1; h->next != NULL; i++)
+	for (i = 0; h; i++)
 		h = h->next;
 	return (i);
 }
