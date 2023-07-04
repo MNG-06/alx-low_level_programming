f3a9ea95019d983f328d7aadcf4687334e75982a
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-07-05 00:32:24 +0200

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,16 +12,15 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *popped;
-	int content;
+	int n;
+	listint_t *next;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
-	popped = *head;
-	content = popped->n;
-	free(popped);
-	*head = (*head)->next;
-	
-	return (content);
+	n = (*head)->n;
+	next = (*head)->next;
+	free(*head);
+	*head = next;
+	return (n);
 }
