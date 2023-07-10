0ad33370dd1f1df5a77607ec5f779eef365e8a4d
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-07-10 11:28:26 +0200

    Update 0-putchar.c

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
+/*#include <stdio.h>*/
 #include <unistd.h>
-#include "main.h"
+/*#include "main.h"*/
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
+ * _putchar - Write char c to stdout.
+ * @c: The character to print.
+ * Return: 1 on success.
+ * -1 is returned on error, and error is set appropriately.
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
