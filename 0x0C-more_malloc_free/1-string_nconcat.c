f624dab7897fc0ba6bf6edce78deb68aca53dc33
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-07-14 16:44:48 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/*unsigned int length(char *s1);*/
+unsigned int length(char *s1);
 
 /**
  * string_nconcat - A function that concatenates two strings.
@@ -12,45 +12,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	cnt i;
-	unsigned int j;
-	unsigned int len;
-	char *heap_array;
+	unsigned int i, j, k;
+	char *ptr = NULL;
 
-	/* Initializing to an empty string if value is NULL*/
 	if (s1 == NULL)
 		s1 = "";
-	if (s1 == NULL)
+	if (s2 == NULL)
 		s2 = "";
-	len = length(s1);
-	heap_array = malloc((len + n + 1) * sizeof(char));
-	if (heap_array == NULL)
-		return (NULL);
-	len = length(s2);
-	if (n >= len)
-		n = len;
-	/*Populating the new array with values*/
+
 	for (i = 0; s1[i] != '\0'; i++)
-		heap_array[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		heap_array[i] = s2[j];
-	heap_array[i] = '\0';
-	return (heap_array);
-}
+		;
 
-/**
- * length - Calculates the length of a string
- * @s1: The string
- * Return: The length of a string as an integer
- */
-unsigned int length(char *s1)
-{
-	unsigned int i;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	if (s1 == NULL)
-		return (0);
-	for (i = 0; s1[i] !=  '\0'; i++)
-	{}
+	if (j <= n)
+		n = j;
 
-	return (i);
+	ptr = malloc((i + n + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (k = 0; s1[k] != '\0'; k++)
+		ptr[k] = s1[k];
+
+	for (k = 0; k < n; k++)
+		ptr[k + i] = s2[k];
+
+	ptr[k + i] = '\0';
+
+	return (ptr);
 }
