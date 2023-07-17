0c23186272ebfa823ac0cfd160e746e4b1853c14
MNG-06 <mphela.mminanare@gmail.com>
2023-07-17 22:31:52 +0200

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the original file.
+ * @void: Nothing
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
