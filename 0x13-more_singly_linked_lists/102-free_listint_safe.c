913f325062fe78e42b1b4bc191effb1885f5231f
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-08-01 23:02:10 +0200

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,11 +3,31 @@
 
 /**
   * free_listint_safe - function that frees a listint_t.
-  * @h:
+  * @h:input head of linked list
   * Return: what does it return?
   */
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	size_t i = 0, j;
+	
+	while (*h)
+	{
+		i++;
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		check = copy;
+		j = 0;
+		while (j < i)
+		{
+			if (*h == check)
+			{
+				*h = NULL;
+				return (i);
+			}
+			check = check->next;
+			j++;
+		}
+	}
+	return (i);
 }
