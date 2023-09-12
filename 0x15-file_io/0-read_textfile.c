4664d6f121c043fe98b7260b01092b471e3141f8
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2023-09-12 12:03:16 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,44 +7,30 @@
  * Return: The actual number of letters it could read and print.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	int i;
-	int read_letters;
-	char *buf;
-
-	if (filename == NULL)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
-	read_letters = read(fd, buf, letters);
-	if (read_letters == -1)
-	{
-		close(fd);
-		free(buf);
-		return (0);
-	}
-
-	for (i = 0; i < read_letters; i++)
-	{
-		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
-		{
-			close(fd);
-			free(buf);
-			return (0);
-		}
-
-	}
-	close(fd);
-	free(buf);
-	return (read_letters);
-}
+ssize_t read_textfile(const char *filename, size_t letters) 
+ { 
+         int fd; 
+         ssize_t nrd, nwr; 
+         char *buf; 
+  
+         if (!filename) 
+                 return (0); 
+  
+         fd = open(filename, O_RDONLY); 
+  
+         if (fd == -1) 
+                 return (0); 
+  
+         buf = malloc(sizeof(char) * (letters)); 
+         if (!buf) 
+                 return (0); 
+  
+         nrd = read(fd, buf, letters); 
+         nwr = write(STDOUT_FILENO, buf, nrd); 
+  
+         close(fd); 
+  
+         free(buf); 
+  
+         return (nwr); 
+ }
